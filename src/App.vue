a21a5a45dc1dc5103f85c521805b5ec705e09c41
louiss0 <louisshelton0@gmail.com>
2022-08-13 08:57:20 -0400

    finished implementing gap logic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import Gap from "@/components/Gap.vue"
+import Gap from './components/Gap.vue';
 </script>
 
 <template>
-    <div class="flex h-24">
-    <div class="bg-blue-900 w-24 h-full"></div>
-    <Gap :spaces="3" class="bg-red-500" />
-    <div class="bg-blue-900 w-24 h-full"></div>
-    <Gap :spaces="3" class="bg-red-500" />
-    <div class="bg-blue-900 w-24 h-full"></div>
-    <Gap :spaces="3" class="bg-red-500" />
-    </div>
+  <div>
+    <div class="bg-blue-900 w-24 h-24"></div>
+    <Gap :spaces="3" />
+    <div class="bg-blue-900 w-24 h-32"></div>
+    <Gap :spaces="6" />
+    <div class="bg-blue-900 w-24 h-24"></div>
+  </div>
 </template>
-
--- src/components/Gap.vue
+++ src/components/Gap.vue
@@ -1,99 +1,116 @@
 <script lang="ts" setup>
-
-import {onMounted, useAttrs, shallowRef, } from "vue"
+import { onMounted, useAttrs, shallowRef } from 'vue';
 
 interface Props {
-  spaces:number 
+  spaces: number;
+  color?: string;
 }
 
-const gapRef = shallowRef<HTMLDivElement | null>(null)
+const props = defineProps<Props>();
 
-const attrs = useAttrs()
-const id = `gap${Math.ceil(Math.random() * Date.now())}`
+const gapRef = shallowRef<HTMLDivElement | null>(null);
 
-onMounted(()=>{
+const id = `gap${Math.ceil(Math.random() * Date.now())}`;
+
+onMounted(() => {
+  const parentElement = gapRef?.value?.parentElement;
+
+  if (!parentElement && !gapRef.value) return;
+
+  const parentElementStyle = getComputedStyle(parentElement);
+
+  const { value: element } = gapRef;
+
+  changeElementWidthIfTheParentElementFulfilsTheseCOnditions(
+    parentElementStyle,
+    element
+  );
+
+  changeHeightIfTheFollowingConditionsAreTrue(parentElementStyle, element);
+
+  element.style.backgroundColor = props.color;
   
-  
-  
-  const parentElement = gapRef?.value?.parentElement
-  
-  if(parentElement && gapRef.value){
-  
-   const {
-     display:parentElementDisplay, 
-     fontSize: parentElementFontSize,
-     gridAutoFlow:parentElementGridAutoFlow,
-     flexDirection:parentElementFlexDirection,
-    } = getComputedStyle(parentElement)
-  
-  
-    const {value:element} = gapRef
-  
-    const parentElementsDisplayIncludesInline = 
-    parentElementDisplay.includes("inline")
-  
-  
-    const parentElementsDisplayIsFlex =
-   parentElementDisplay.includes("flex")
-  
-   const parentElementStyleFlexDirectionIsRow= 
-   parentElementFlexDirection.includes("row")
-  
-   const parentElementStyleFlexDirectionIsColumn= 
-   parentElementFlexDirection.includes("column")
-  
-  
-   const parentElementsDisplayIsGrid =
-   parentElementDisplay.includes("grid")
-  
-  
-   const parentElementStyleGridAutoFlowIsRow= 
-    parentElementGridAutoFlow.includes("row")
-  
-   const parentElementStyleGridAutoFlowIsColumn= 
-    parentElementGridAutoFlow.includes("column")
-    
-  
-   const anyOfTheConditionsRequiredToSetTheHeightOfTheElementAreTrue = [
-     parentElementsDisplayIsGrid && parentElementStyleGridAutoFlowIsRow,
-     parentElementsDisplayIsFlex && parentElementStyleFlexDirectionIsColumn
-    ].some((value)=> value) 
-    
-    const anyOfTheConditionsRequiredToSetTheWidthOfTheElementAreTrue =  [
-      parentElementsDisplayIncludesInline,
-      parentElementsDisplayIsFlex && parentElementStyleFlexDirectionIsRow,
-      parentElementsDisplayIsGrid && parentElementStyleGridAutoFlowIsColumn,
-  ].some((value)=> value) 
-  
-    if(anyOfTheConditionsRequiredToSetTheWidthOfTheElementAreTrue) {
-  
-  
-    element.style.width = `${parseInt(parentElementFontSize) * props.spaces}px`
-  
-    element.style.height = "auto"
-  
-    } else if(anyOfTheConditionsRequiredToSetTheHeightOfTheElementAreTrue) {
-      
-      
-      element.style.width = "auto"
-      element.style.height = 
-      `${parseInt(parentElementFontSize) * props.spaces}px`
-  
-  
-    }
-  
-  
-    
+});
+
+function changeElementWidthIfTheParentElementFulfilsTheseCOnditions(
+  parentElementStyle,
+  element: HTMLElement
+) {
+  const {
+    display: parentElementDisplay,
+    fontSize: parentElementFontSize,
+    gridAutoFlow: parentElementGridAutoFlow,
+    flexDirection: parentElementFlexDirection,
+  } = parentElementStyle;
+
+  const parentElementsDisplayIsFlex = parentElementDisplay.includes('flex');
+
+  const parentElementsDisplayIsGrid = parentElementDisplay.includes('grid');
+
+  const parentElementsDisplayIncludesInline =
+    parentElementDisplay.includes('inline');
+
+  const parentElementStyleFlexDirectionIsRow =
+    parentElementFlexDirection.includes('row');
+
+  const parentElementStyleGridAutoFlowIsColumn =
+    parentElementGridAutoFlow.includes('column');
+
+  const anyOfTheConditionsRequiredToSetTheWidthOfTheElementAreTrue = [
+    parentElementsDisplayIncludesInline,
+    parentElementsDisplayIsFlex && parentElementStyleFlexDirectionIsRow,
+    parentElementsDisplayIsGrid && parentElementStyleGridAutoFlowIsColumn,
+  ].some((value) => value);
+
+  if (anyOfTheConditionsRequiredToSetTheWidthOfTheElementAreTrue) {
+    element.style.width = `${parseInt(parentElementFontSize) * props.spaces}px`;
+
+    element.style.height = 'auto';
   }
-  
+}
+
+function changeHeightIfTheFollowingConditionsAreTrue(
+  parentElementStyle,
+  element: HTMLElement
+) {
+  const {
+    display: parentElementDisplay,
+    fontSize: parentElementFontSize,
+    gridAutoFlow: parentElementGridAutoFlow,
+    flexDirection: parentElementFlexDirection,
+  } = parentElementStyle;
+  const parentElementsDisplayIsFlex = parentElementDisplay.includes('flex');
+
+  const parentElementsDisplayIsGrid = parentElementDisplay.includes('grid');
+
+  const parentDisplayIsBlock = parentElementDisplay.includes('block');
+  const parentElementStyleFlexDirectionIsColumn =
+    parentElementFlexDirection.includes('column');
+
+  const parentElementStyleGridAutoFlowIsRow =
+    parentElementGridAutoFlow.includes('row');
+
+  const anyOfTheConditionsRequiredToSetTheHeightOfTheElementAreTrue = [
+    parentDisplayIsBlock,
+    parentElementsDisplayIsGrid && parentElementStyleGridAutoFlowIsRow,
+    parentElementsDisplayIsFlex && parentElementStyleFlexDirectionIsColumn,
+  ].some((value) => value);
+
+  if (anyOfTheConditionsRequiredToSetTheHeightOfTheElementAreTrue) {
+    element.style.width = 'auto';
+    element.style.height = `${
+      parseInt(parentElementFontSize) * props.spaces
+    }px`;
   }
-)
+}
 </script>
 
-
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
 
 <template>
- <div :ref="gapRef" :id="id" v-bind="{...attrs}"> </div>
+  <div ref="gapRef" :id="id"></div>
 </template>
-
-
